54160a89c64389e5d7d931d452cd79b491448939
kyunggwan <4562sky@naver.com>
2023-10-05 15:57:59 +0900

    feat: 공통 객체 타입 설정

---
+++ board-back/src/main/java/com/pkk/boardback/dto/object/BoardListItem.java
@@ -0,0 +1,24 @@
+package com.pkk.boardback.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardListItem {
+
+    private int boardNumber;
+    private String title;
+    private String content;
+    private String boardTitleImage;
+    private int favoriteCount;
+    private int commentCount;
+    private int viewCount;
+    private String writeDateTime;
+    private String writerNickname;
+    private String writerProfileImage;
+    
+}
