2a2dd3466e7430903e65f00880e411077e2de1df
kyunggwan <4562sky@naver.com>
2023-10-06 17:24:55 +0900

    feat: top3listItem 컴포넌트 생성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,13 +1,19 @@
 import BoardItem from "components/BoardItem";
 import "./App.css";
 import latestBoardListMock from "mocks/latest-board-list.mocks";
+import Top3Item from "components/Top3Item";
+import top3BoardListMock from "mocks/top-3-list.mocks";
 
 function App() {
   return (
     <>
-      {latestBoardListMock.map((boardListItem) => (
+      <div style={{ display: "flex", justifyContent: "center", gap: "24px" }}>
+        {/* {latestBoardListMock.map((boardListItem) => (
         <BoardItem boardListItem={boardListItem} />
-      ))}
+      ))} */}
+        {/* <Top3Item /> */}
+        {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
+      </div>
     </>
   );
 }
--- board-front/src/mocks/index.tsx
+++ board-front/src/mocks/index.tsx
@@ -1,5 +1,7 @@
 import latestBoardListMock from "./latest-board-list.mocks";
+import top3BoardListMock from "./top-3-list.mocks";
 
 export default {
   latestBoardListMock,
+  top3BoardListMock,
 };
