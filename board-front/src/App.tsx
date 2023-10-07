31b1ad846f3e9f72504ac449f4d2bcde85e48e89
kyunggwan <4562sky@naver.com>
2023-10-07 17:36:48 +0900

    feat: input컴포넌트 생성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -3,16 +3,13 @@ import "./App.css";
 import latestBoardListMock from "mocks/latest-board-list.mocks";
 import Top3Item from "components/Top3Item";
 import top3BoardListMock from "mocks/top-3-list.mocks";
+import InputBox from "components/InputBox";
 
 function App() {
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "center", gap: "24px" }}>
-        {/* {latestBoardListMock.map((boardListItem) => (
-        <BoardItem boardListItem={boardListItem} />
-      ))} */}
-        {/* <Top3Item /> */}
-        {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
+      <div >
+        <InputBox />
       </div>
     </>
   );
