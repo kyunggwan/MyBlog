3158f7a99790a97210f57fbc80c0febac2bf007a
kyunggwan <4562sky@naver.com>
2023-10-08 23:13:24 +0900

    feat: footer layout추가중

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,19 +1,12 @@
-import BoardItem from "components/BoardItem";
+import Footer from "layouts/Footer";
 import "./App.css";
-import latestBoardListMock from "mocks/latest-board-list.mocks";
-import Top3Item from "components/Top3Item";
-import top3BoardListMock from "mocks/top-3-list.mocks";
-import InputBox from "components/InputBox";
 import { useState } from "react";
 
 function App() {
 
-  const [value, setvalue] = useState<string>("");
   return (
     <>
-      <div >
-        <InputBox label="이메일" type='text' placeholder="이메일 주소를 입력해주세요" value={value} error={true} setValue={setvalue} message="" />
-      </div>
+      <Footer />
     </>
   );
 }
