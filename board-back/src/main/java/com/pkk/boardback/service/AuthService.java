2924bd8cd8e04b0fd0ea53803e098b6ff26f6956
kyunggwan <4562sky@naver.com>
2023-11-05 22:39:51 +0900

    feat: 로그인 기능 구현

--- board-back/src/main/java/com/pkk/boardback/service/AuthService.java
+++ board-back/src/main/java/com/pkk/boardback/service/AuthService.java
@@ -2,10 +2,14 @@ package com.pkk.boardback.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.pkk.boardback.dto.request.auth.SignInRequestDto;
 import com.pkk.boardback.dto.request.auth.SignUpRequestDto;
+import com.pkk.boardback.dto.response.auth.SignInResponseDto;
 import com.pkk.boardback.dto.response.auth.SignUpResponseDto;
 
 public interface AuthService {
 
     ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto);
+
+    ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto);
 }
--- board-back/src/main/java/com/pkk/boardback/service/implement/AuthServiceImplements.java
+++ board-back/src/main/java/com/pkk/boardback/service/implement/AuthServiceImplements.java
@@ -5,10 +5,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.pkk.boardback.dto.request.auth.SignInRequestDto;
 import com.pkk.boardback.dto.request.auth.SignUpRequestDto;
 import com.pkk.boardback.dto.response.ResponseDto;
+import com.pkk.boardback.dto.response.auth.SignInResponseDto;
 import com.pkk.boardback.dto.response.auth.SignUpResponseDto;
 import com.pkk.boardback.entity.UserEntity;
+import com.pkk.boardback.provider.JwtProvider;
 import com.pkk.boardback.repository.UserRepository;
 import com.pkk.boardback.service.AuthService;
 
@@ -19,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthServiceImplements implements AuthService {
 
     private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -53,6 +57,33 @@ public class AuthServiceImplements implements AuthService {
             return ResponseDto.databaseError();
         }
         return SignUpResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto) {
+
+        String token = null;
+
+        try {
+            String email = dto.getEmail();
+            UserEntity userEntity = userRepository.findByEmail(email);
+            if (userEntity == null)
+                return SignInResponseDto.signInFail();
+
+            String password = dto.getPassword();
+            String encodedPassword = userEntity.getPassword();
+            boolean isMatched = passwordEncoder.matches(password, encodedPassword);
+
+            if (!isMatched)
+                return SignInResponseDto.signInFail();
+
+            token = jwtProvider.create(email);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return SignInResponseDto.success(token);
     }
 
     /*
