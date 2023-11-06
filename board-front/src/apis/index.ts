2d4e98c768958d5d139639dd30d9c12dd3025007
kyunggwan <4562sky@naver.com>
2023-11-07 00:08:59 +0900

    feat: 로그인 api 생성 중

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -1,1 +1,16 @@
-export {}
+import { SignInRequestDto, SignUpRequestDto } from "./requeset/auth";
+
+const DOMAIN = "http://localhost:4000";
+
+const API_DOMAIN = `${DOMAIN}/api/v1`;
+
+const SIGN_IN_URL = () => { `${API_DOMAIN}/auth/sign-in` };
+const SIGN_UP_URL = () => { `${API_DOMAIN}/auth/sign-up` };
+
+export const signInRequest = (requestbody: SignInRequestDto) => {
+
+}
+
+export const signUpRequest = (requestbody: SignUpRequestDto) => {
+
+}
