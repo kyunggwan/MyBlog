330cebf8c3d634129f050aa05e37670a13140453
kyunggwan <4562sky@naver.com>
2023-11-29 23:10:40 +0900

    feat: 게시물 작성 api 연동

--- board-front/src/layouts/Header/index.tsx
+++ board-front/src/layouts/Header/index.tsx
@@ -5,6 +5,10 @@ import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRIT
 import { useCookies } from 'react-cookie';
 import { useBoardStore, useLoginUserStore } from 'stores';
 import { log } from 'console';
+import { fileUploadRequest, postBoardRequest } from 'apis';
+import { PostBoardRequestDto } from 'apis/requeset/board';
+import { PostBoardResponseDto } from 'apis/response/board';
+import { ResponseDto } from 'apis/response';
 
 //              component: 헤더 레이아웃                //
 export default function Header() {
@@ -14,7 +18,7 @@ export default function Header() {
     //              state: path 상태                //
     const { pathname } = useLocation();
     //              state: cookie 상태              //
-    const [cookie, setCookie] = useCookies();
+    const [cookies, setCookie] = useCookies();
     //              state: 로그인 상태              //
     const [isLogin, setLogin] = useState<boolean>(false);
     //              state: 인증 페이지 상태             //
@@ -122,6 +126,7 @@ export default function Header() {
         //              event handler: 마이페이지 버튼 클릭 이벤트 처리 함수                //
         const onSignInButtonClickHandler = () => {
             navigate(AUTH_PATH());
+            return;
         }
 
         //              render: 로그인 아웃버튼 컴포넌트 렌더링                //
@@ -152,9 +157,39 @@ export default function Header() {
         //              state: 게시물 상태                //
         const { title, content, boardImgFileList, resetBoard } = useBoardStore();
 
-        //              event handler: 업로드 버튼 클릭 이벤트 처리 함수              //
-        const onUploadButtonClickHandler = () => {
+        //              function: post board response 처리 함수             //
+        const postBoardResponse = (responseBody: PostBoardResponseDto | ResponseDto | null) => {
+            if (!responseBody) return;
+            const { code } = responseBody;
+            if (code === 'DBE') alert('데이터베이스 오류입니다.');
+            if (code === 'AF' || code === 'NU') navigate(AUTH_PATH());
+            if (code === 'VF') alert('제목과 내용은 필수입니다.');
+            if (code !== 'SU') return;
 
+            resetBoard();
+            if (!loginUser) return;
+            const { email } = loginUser;
+            navigate(USER_PATH(email));
+        }
+        //              event handler: 업로드 버튼 클릭 이벤트 처리 함수              //
+        const onUploadButtonClickHandler = async () => {
+            const accessToken = cookies.accessToken;
+            if (!accessToken) return;
+
+            const boardImageList: string[] = [];
+            for (const file of boardImgFileList) {
+                const data = new FormData();
+                data.append('file', file);
+
+                const url = await fileUploadRequest(data);
+                if (url) boardImageList.push(url);
+            }
+
+            const requestBody: PostBoardRequestDto = {
+                title, content, boardImageList
+            }
+            postBoardRequest(requestBody, accessToken)
+                .then(postBoardResponse);
         }
         //              render: 업로드 버튼 컴포넌트 렌더링                //
         if (title && content)
