f6b9e4a759b7f88a0d6d455c9b87c706ef92223a
kyunggwan <4562sky@naver.com>
2023-11-29 21:55:04 +0900

    feat: 게시물 작성 페이지 이벤트 처리 - 미리보기 이미지 업로드, 이미지 삭제

--- board-front/src/views/Board/Write/index.tsx
+++ board-front/src/views/Board/Write/index.tsx
@@ -4,6 +4,9 @@ import { useBoardStore } from 'stores';
 
 //              component: 게시물 작성 화면 컴포넌트               //
 export default function BoardWrite() {
+
+    //              state: 제목 영역 요소 참조 상태               //
+    const titleRef = useRef<HTMLTextAreaElement | null>(null);
     //              state: 본문 영역 요소 참조 상태               //
     const contentRef = useRef<HTMLTextAreaElement | null>(null);
     //              state: 이미지 입력 요소 참조 상태               //
@@ -18,8 +21,62 @@ export default function BoardWrite() {
     const [imageUrls, setImageUrls] = useState<string[]>([]);
 
     //              event handler: 제목 변경 이벤트 처리                //
-    const onTitleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const onTitleChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
         const { value } = event.target;
+        setTitle(value);
+
+        if (!titleRef.current) return;
+        titleRef.current.style.height = 'auto';
+        titleRef.current.style.height = `${titleRef.current.scrollHeight}px`;
+    }
+
+    //              event handler: 내용 변경 이벤트 처리                //
+    const onContentChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const { value } = event.target;
+        setContent(value);
+
+        if (!contentRef.current) return;
+        contentRef.current.style.height = 'auto';
+        contentRef.current.style.height = `${contentRef.current.scrollHeight}px`;
+    }
+
+    //              event handler: 이미지 변경 이벤트 처리              //
+    const onImageChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        if (!event.target.files || !event.target.files.length) return;
+        const file = event.target.files[0];
+
+        // 미리보기용 url
+        const imageUrl = URL.createObjectURL(file);
+        const newImageUrls = imageUrls.map(item => item);
+        newImageUrls.push(imageUrl);
+        setImageUrls(newImageUrls);
+
+        // db 업로드용
+        const newBoardImageFileList = boardImgFileList.map(item => item);
+        newBoardImageFileList.push(file);
+        setBoardImgFileList(newBoardImageFileList);
+    }
+
+    //              event handler: 이미지 업로드 버튼 클릭 이벤트 처리              //
+    const onImageUploadButtonClickHandler = () => {
+        if (!imageInputRef.current) return;
+        imageInputRef.current.click();
+    }
+
+    //              event handler: 이미지 닫기 버튼 클릭 이벤트 처리              //
+    const onImageCloseButtonClickHandler = (deleteIndex: number) => {
+        if (!imageInputRef.current) return;
+        imageInputRef.current.value = '';
+
+        const newImageUrls = imageUrls.filter((url, index) => index !== deleteIndex);
+        setImageUrls(newImageUrls);
+
+        const newBoardImageFileList = boardImgFileList.filter((file, index) => index !== deleteIndex);
+        setBoardImgFileList(newBoardImageFileList);
+
+        // 같은 이미지 추가되게
+        if (!imageInputRef.current) return;
+        imageInputRef.current.value = '';
     }
 
     //              effect: 마운트 시 실행할 함수               //
@@ -33,36 +90,28 @@ export default function BoardWrite() {
             <div className='board-write-container'>
                 <div className='board-write-box'>
                     <div className='board-wirte-title-box'>
-                        <input className='board-write-title-input' type='text' placeholder='제목을 작성해주세요.' value={title} />
+                        <textarea ref={titleRef} className='board-write-title-textarea' rows={1} placeholder='제목을 작성해주세요.' value={title} onChange={onTitleChangeHandler} />
                     </div>
                     <div className='divider'></div>
                     <div className='board-write-content-box'>
-                        <textarea ref={contentRef} className='board-write-content-textarea' placeholder='본문을 작성해주세요.' value={content} />
-                        <div className='icon-button'>
+                        <textarea ref={contentRef} className='board-write-content-textarea' placeholder='본문을 작성해주세요.' value={content} onChange={onContentChangeHandler} />
+                        <div className='icon-button' onClick={onImageUploadButtonClickHandler}>
                             <div className='icon image-box-light-icon'></div>
                         </div>
-                        <input ref={imageInputRef} type='file' accept='image/*' style={{ display: 'none' }} />
+                        <input ref={imageInputRef} type='file' accept='image/*' style={{ display: 'none' }} onChange={onImageChangeHandler} />
                     </div>
-                    <div className='board-write-images-box'>
-                        <div className='board-write-image-box'>
-                            <img className='board-write-image' src='https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_23F8B01DC9DCBE56216EED03018D4C7D.jpg&type=sc960_832' />
-                            <div className='icon-button image-close'>
-                                <div className='icon close-icon'></div>
+                    {imageUrls.map((imageUrl, index) =>
+                        <div className='board-write-images-box'>
+                            <div className='board-write-image-box'>
+                                <img className='board-write-image' src={imageUrl} />
+                                <div className='icon-button image-close' onClick={() => onImageCloseButtonClickHandler(index)}>
+                                    <div className='icon close-icon'></div>
+                                </div>
                             </div>
                         </div>
+                    )}
 
-                    </div>
-                    <div className='board-write-images-box'>
-                        <div className='board-write-image-box'>
-                            <img className='board-write-image' src='https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FBF1146EF4F1BEF77E19827EDE16CC42.jpeg&type=sc960_832' />
-                            <div className='icon-button image-close'>
-                                <div className='icon close-icon'></div>
-                            </div>
-                        </div>
-
-                    </div>
                 </div>
-
             </div>
         </div>
     )
