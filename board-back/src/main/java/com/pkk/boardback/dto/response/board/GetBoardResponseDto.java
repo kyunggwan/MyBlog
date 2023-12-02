e16bfbe825c431476200404336527b04a06cb9be
kyunggwan <4562sky@naver.com>
2023-12-02 23:56:57 +0900

    feat: GetBoardResponseDto 생성

---
+++ board-back/src/main/java/com/pkk/boardback/dto/response/board/GetBoardResponseDto.java
@@ -0,0 +1,39 @@
+package com.pkk.boardback.dto.response.board;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.pkk.boardback.common.ResponseCode;
+import com.pkk.boardback.common.ResponseMessage;
+import com.pkk.boardback.dto.response.ResponseDto;
+
+import lombok.Getter;
+
+@Getter
+public class GetBoardResponseDto extends ResponseDto {
+
+    private int boardNumber;
+    private String title;
+    private String content;
+    private List<String> boardImageList;
+    private String writeDateTime;
+    private String writerEmail;
+    private String writerNickname;
+    private String writerProfileImage;
+
+    private GetBoardResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<GetBoardResponseDto> success() {
+        GetBoardResponseDto result = new GetBoardResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistBoard() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_BOARD, ResponseMessage.NOT_EXISTED_BOARD);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+}
