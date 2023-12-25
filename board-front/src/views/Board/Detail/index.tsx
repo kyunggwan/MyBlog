5881dae0b4d0d074876cd46cbc4a0b78852657f4
kyunggwan <4562sky@naver.com>
2023-12-25 22:47:27 +0900

    style: 게시물 상세보기 상단 컴포넌트 스타일 수정

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css';
+import defaultProfileImage from 'assets/image/default-profile-image.png';
 
 //              component: 게시물 상세 화면 컴포넌트               //
 export default function BoardDetail() {
@@ -7,30 +8,42 @@ export default function BoardDetail() {
 
     //              component: 게시물 상세 상단 컴포넌트               //
     const BoardDetailTop = () => {
+
+        //              state: nire 버튼 상태               //
+        const [showMore, setShowMore] = useState<boolean>(false);
+
+        //              event handler: more 버튼 클릭 이벤트 처리               //
+        const onMoreButtonClickHandler = () => {
+            setShowMore(!showMore);
+        }
         return (
             <div id='board-detail-top'>
                 <div className='board-detail-top-header'>
                     <div className='board-detail-title'>{'title'}</div>
                     <div className='board-detail-top-sub-box'>
                         <div className='board-detil-write-info-box'></div>
-                        <div className='board-detail-writer-profile-image'></div>
+                        <div className='board-detail-writer-profile-image' style={{ backgroundImage: `url(${defaultProfileImage})` }}></div>
                         <div className='board-detail-writer-nickname'>{'nickname'}</div>
                         <div className='board-detail-writer-info-divider'>{'/'}</div>
                         <div className='board-detail-write-date'>{'날짜'}</div>
                     </div>
-                    <div className='icon-button'>
+                    <div className='icon-button' onClick={onMoreButtonClickHandler}>
                         <div className='icon more-icon'></div>
                     </div>
-                    <div className='board-detail-more-box'>
+                    {showMore && <div className='board-detail-more-box'>
                         <div className='board-detail-update-button'>{'수정'}</div>
                         <div className='divider'>{'/'}</div>
                         <div className='board-detail-delete-button'>{'삭제'}</div>
                     </div>
+                    }
+
                 </div>
                 <div className='divider'></div>
                 <div className='board-detail-top-main'>
                     <div className='board-detail-main-text'>{'main text'}</div>
-                    <div className='board-detail-main-image'>{'main image'}</div>
+                    {/* <div className='board-detail-main-image' style={{backgroundImage=`url(${})`}}> */}
+
+                    {/* </div> */}
                 </div>
             </div>
         );
@@ -39,17 +52,17 @@ export default function BoardDetail() {
     //              component: 게시물 상세 하단 컴포넌트               //
     const BoardDetailBottom = () => {
         return (
-        <div className='board-detail-bottom'>
-            <div className='board-detail-bottom-button-box'>
-                <div className='board-detail-bottom-button-group'>
-                    <div className='icon-button'>
-                        <div className='icon favorite-fill-icon'></div>
+            <div className='board-detail-bottom'>
+                <div className='board-detail-bottom-button-box'>
+                    <div className='board-detail-bottom-button-group'>
+                        <div className='icon-button'>
+                            <div className='icon favorite-fill-icon'></div>
+                        </div>
                     </div>
                 </div>
+                <div className='board-detail-bottom-favoeite-box'></div>
+                <div className='board-detail-bottom-comment-box'></div>
             </div>
-            <div className='board-detail-bottom-favoeite-box'></div>
-            <div className='board-detail-bottom-comment-box'></div>
-        </div>
         );
     }
 
