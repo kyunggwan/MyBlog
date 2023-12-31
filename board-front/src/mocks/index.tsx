8ed8ac8e8820bba8c89e032f16b0102ef90757ca
kyunggwan <4562sky@naver.com>
2023-12-31 21:37:16 +0900

    feat: comment-list 목 추가

--- board-front/src/mocks/index.tsx
+++ board-front/src/mocks/index.tsx
@@ -1,9 +1,11 @@
 import favoriteListMock from "./favorite-list.mock";
 import latestBoardListMock from "./latest-board-list.mocks";
 import top3BoardListMock from "./top-3-list.mocks";
+import commentListMock from "./comment-list.mock";
 
 export default {
   latestBoardListMock,
   top3BoardListMock,
   favoriteListMock,
+  commentListMock,
 };
