f2998d05b3646c63a979d13919a828d5d72b0825
kyunggwan <4562sky@naver.com>
2023-12-31 21:00:13 +0900

    feat: 빈 컴포넌트 생성

---
+++ board-front/src/components/FavoriteItem/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FavoriteListItem } from "types/interface";
+import defaultProfileImage from "assets/image/default-profile-image.png";
+// import "./styles.css";
+
+interface Props {
+  favoriteListItem: FavoriteListItem;
+}
+
+//              component: Favorite List Item 컴포넌트              //
+export default function FavoriteItem({ favoriteListItem }: Props) {
+  //              state: properties               //
+  const { profileImage, nickname } = favoriteListItem;
+
+  //              render: Favorite List Item 렌더링               //
+  return (
+    <div className="favorite-list-item">
+      <div className="favorite-list-item-profile-box">
+        <div className="favorite-list-item-profile-image"></div>
+      </div>
+      <div className="favorite-list-item-nickname">{nickname}</div>
+    </div>
+  );
+}
