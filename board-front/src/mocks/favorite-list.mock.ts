e2a190e22fb8294552bf7c8845b1306630f782c3
kyunggwan <4562sky@naver.com>
2023-12-31 21:00:29 +0900

    feat: 좋아요 목록 mock 생성

---
+++ board-front/src/mocks/favorite-list.mock.ts
@@ -0,0 +1,34 @@
+const favoriteListMock = [
+    {
+        "email": "email@email.com",
+        "nickname": "닉네임",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "닉네임",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "닉네임",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "닉네임",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "닉네임",
+        "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "닉네임",
+        "profileImage": null
+    }
+]
+
+export default favoriteListMock;
--- board-front/src/mocks/index.tsx
+++ board-front/src/mocks/index.tsx
@@ -1,7 +1,9 @@
+import favoriteListMock from "./favorite-list.mock";
 import latestBoardListMock from "./latest-board-list.mocks";
 import top3BoardListMock from "./top-3-list.mocks";
 
 export default {
   latestBoardListMock,
   top3BoardListMock,
+  favoriteListMock,
 };
