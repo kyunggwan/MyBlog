a5f86bd11f8987fce24ffa0c9ee9ed80e33dbf82
kyunggwan <4562sky@naver.com>
2023-12-31 20:59:28 +0900

    feat: 게시물 상세보기 구조 작성

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -1,78 +1,135 @@
-import React, { useState } from 'react'
-import './styles.css';
-import defaultProfileImage from 'assets/image/default-profile-image.png';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import defaultProfileImage from "assets/image/default-profile-image.png";
+import FavoriteItem from "components/FavoriteItem";
+import { CommentListItem, FavoriteListItem } from "types/interface";
+import favoriteListMock from "../../../mocks/index";
+import Pagination from "components/Pagination";
 
 //              component: 게시물 상세 화면 컴포넌트               //
 export default function BoardDetail() {
+  //              component: 게시물 상세 상단 컴포넌트               //
+  const BoardDetailTop = () => {
+    //              state: more 버튼 상태               //
+    const [showMore, setShowMore] = useState<boolean>(false);
 
-
-    //              component: 게시물 상세 상단 컴포넌트               //
-    const BoardDetailTop = () => {
-
-        //              state: nire 버튼 상태               //
-        const [showMore, setShowMore] = useState<boolean>(false);
-
-        //              event handler: more 버튼 클릭 이벤트 처리               //
-        const onMoreButtonClickHandler = () => {
-            setShowMore(!showMore);
-        }
-        return (
-            <div id='board-detail-top'>
-                <div className='board-detail-top-header'>
-                    <div className='board-detail-title'>{'title'}</div>
-                    <div className='board-detail-top-sub-box'>
-                        <div className='board-detil-write-info-box'></div>
-                        <div className='board-detail-writer-profile-image' style={{ backgroundImage: `url(${defaultProfileImage})` }}></div>
-                        <div className='board-detail-writer-nickname'>{'nickname'}</div>
-                        <div className='board-detail-writer-info-divider'>{'/'}</div>
-                        <div className='board-detail-write-date'>{'날짜'}</div>
-                    </div>
-                    <div className='icon-button' onClick={onMoreButtonClickHandler}>
-                        <div className='icon more-icon'></div>
-                    </div>
-                    {showMore && <div className='board-detail-more-box'>
-                        <div className='board-detail-update-button'>{'수정'}</div>
-                        <div className='divider'>{'/'}</div>
-                        <div className='board-detail-delete-button'>{'삭제'}</div>
-                    </div>
-                    }
-
-                </div>
-                <div className='divider'></div>
-                <div className='board-detail-top-main'>
-                    <div className='board-detail-main-text'>{'main text'}</div>
-                    {/* <div className='board-detail-main-image' style={{backgroundImage=`url(${})`}}> */}
-
-                    {/* </div> */}
-                </div>
-            </div>
-        );
-    }
-
-    //              component: 게시물 상세 하단 컴포넌트               //
-    const BoardDetailBottom = () => {
-        return (
-            <div className='board-detail-bottom'>
-                <div className='board-detail-bottom-button-box'>
-                    <div className='board-detail-bottom-button-group'>
-                        <div className='icon-button'>
-                            <div className='icon favorite-fill-icon'></div>
-                        </div>
-                    </div>
-                </div>
-                <div className='board-detail-bottom-favoeite-box'></div>
-                <div className='board-detail-bottom-comment-box'></div>
-            </div>
-        );
-    }
-
-    //              render: 게시물 상세 화면 컴포넌트 렌더링               //
+    //              event handler: more 버튼 클릭 이벤트 처리               //
+    const onMoreButtonClickHandler = () => {
+      setShowMore(!showMore);
+    };
+    //              render: 게시물 상세 상단 컴포넌트 렌더링               //
     return (
-        <div id='board-detail-wrapper'>
-            <div className='bord-detail-container'>
-                <BoardDetailTop />
-                <BoardDetailBottom />
+      <div id="board-detail-top">
+        <div className="board-detail-top-header">
+          <div className="board-detail-title">{"title"}</div>
+          <div className="board-detail-top-sub-box">
+            <div className="board-detil-write-info-box"></div>
+            <div
+              className="board-detail-writer-profile-image"
+              style={{ backgroundImage: `url(${defaultProfileImage})` }}
+            ></div>
+            <div className="board-detail-writer-nickname">{"nickname"}</div>
+            <div className="board-detail-writer-info-divider">{"/"}</div>
+            <div className="board-detail-write-date">{"날짜"}</div>
+          </div>
+          <div className="icon-button" onClick={onMoreButtonClickHandler}>
+            <div className="icon more-icon"></div>
+          </div>
+          {showMore && (
+            <div className="board-detail-more-box">
+              <div className="board-detail-update-button">{"수정"}</div>
+              <div className="divider">{"/"}</div>
+              <div className="board-detail-delete-button">{"삭제"}</div>
             </div>
+          )}
         </div>
-    )
+        <div className="divider"></div>
+        <div className="board-detail-top-main">
+          <div className="board-detail-main-text">{"main text"}</div>
+          {/* <div className='board-detail-main-image' style={{backgroundImage=`url(${})`}}> */}
+
+          {/* </div> */}
+        </div>
+      </div>
+    );
+  };
+
+  //              component: 게시물 상세 하단 컴포넌트               //
+  const BoardDetailBottom = () => {
+    const [favoriteList, setFavoriteList] = useState<FavoriteListItem[]>([]);
+    const [commentList, setCommentList] = useState<CommentListItem[]>([]);
+
+    useEffect(() => {
+    //   setFavoriteList(favoriteListMock);
+    // setCommentList(CommentListMock);
+    }, []);
+
+    //              render: 게시물 상세 하단 컴포넌트 렌더링                //
+    return (
+      <div className="board-detail-bottom">
+        <div className="board-detail-bottom-button-box">
+          <div className="board-detail-bottom-button-group">
+            <div className="icon-button">
+              <div className="icon favorite-fill-icon"></div>
+            </div>
+            <div className="board-detail-bottom-button-text">{`좋아요 ${12}`}</div>
+            <div className="icon-button">
+              <div className="icon up-light-icon"></div>
+            </div>
+          </div>
+          <div className="board-detail-bottom-button-group">
+            <div className="icon-button">
+              <div className="icon comment-icon"></div>
+            </div>
+            <div className="board-detail-bottom-button-text">{`댓글 ${12}`}</div>
+            <div className="icon-button">
+              <div className="icon up-light-icon"></div>
+            </div>
+          </div>
+        </div>
+        <div className="board-detail-bottom-favorite-box">
+          <div className="board-detail-bottom-favorite-container">
+            <div className="board-detail-bottom0favorite0title">
+              {"좋아요"} <span className="emphasis">{12}</span>
+            </div>
+            <div className="board0etail-bottom-favorite-contents">
+              {favoriteList.map((item) => (
+                <FavoriteItem favoriteListItem={item} />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="board-detail-bottom-comment-box">
+          <div className="board-detail-bottom-comment-container">
+            <div className="board-detail-bottom-comment-title">{'댓글 '} <span className="emphasis">{12}</span></div>
+            <div className="board-detail-bottom-comment-list-container">
+                {/* {commentList.map(item => <CommentItem commentListItem={item} />)} */}
+            </div>
+          </div>
+          <div className="divider"></div>
+          <div className="board-detail-bottom-comment-pagination-box">
+            <Pagination />
+          </div>
+          <div className="board-detail-bottom-comment-input-container">
+            <div className="board-detail-bottom-comment-input-container">
+                <textarea className="board-detail-bottom-comment-textarea" placeholder="댓글을 작성해주세요."/>
+                <div className="board-detail-bottom-comment-button-box">
+                  <div className="disable-button">{'댓글달기'}</div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  //              render: 게시물 상세 화면 컴포넌트 렌더링               //
+  return (
+    <div id="board-detail-wrapper">
+      <div className="bord-detail-container">
+        <BoardDetailTop />
+        <BoardDetailBottom />
+      </div>
+    </div>
+  );
 }
