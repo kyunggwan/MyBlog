e5b8d87c640e153da9a1beda8f6e15f3de046878
kyunggwan <4562sky@naver.com>
2024-01-02 17:20:35 +0900

    feat: boardMock 데이터 생성

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,18 @@
+import { Board } from "types/interface";
+
+const boardMock: Board = {
+  boardNumber: 1,
+  title: "오늘 뭐먹지..제목",
+  content: "점심메뉴... 내용",
+  boardImageList: [
+    "https://file.mk.co.kr/meet/neds/2022/04/image_readbot_2022_316088_16493769485003005.jpeg",
+    "https://newsimg.sedaily.com/2023/05/01/29PES5TGCX_1.jpg",
+  ],
+  writeDatetime: " 2024.01.02",
+  writerEmail: "4562sky@naver.com",
+  writerNickname: "hannin",
+  writerProfileImage:
+    "https://i.namu.wiki/i/TYxKQDnuwFOcxdSaPR-L81SPQGf5aPEz13tINJ-Z508LKNtGmRmkZTKKEN82SrIZAYoLL8WSbXGzv2PiLgpRSg.webp",
+};
+
+export default boardMock;
