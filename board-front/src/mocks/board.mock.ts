4607d8ef0dd1cd6c992cdecad04b10308f4c4e66
kyunggwan <4562sky@naver.com>
2024-01-02 18:25:39 +0900

    feat: 게시물 상세 페이지 이벤트 처리

--- board-front/src/mocks/board.mock.ts
+++ board-front/src/mocks/board.mock.ts
@@ -3,10 +3,10 @@ import { Board } from "types/interface";
 const boardMock: Board = {
   boardNumber: 1,
   title: "오늘 뭐먹지..제목",
-  content: "점심메뉴... 내용",
+  content: "점심메뉴... 내용입니다. 컨텐츠 내용이라 길게 써야하는데 뭐라 적어놓을지는 모르겠고, 일단 길게 쓰는 중입니다. ",
   boardImageList: [
     "https://file.mk.co.kr/meet/neds/2022/04/image_readbot_2022_316088_16493769485003005.jpeg",
-    "https://newsimg.sedaily.com/2023/05/01/29PES5TGCX_1.jpg",
+    "https://i.namu.wiki/i/TYxKQDnuwFOcxdSaPR-L81SPQGf5aPEz13tINJ-Z508LKNtGmRmkZTKKEN82SrIZAYoLL8WSbXGzv2PiLgpRSg.webp",
   ],
   writeDatetime: " 2024.01.02",
   writerEmail: "4562sky@naver.com",
