00b0169ca12adc2905c3c3266284e0daeecdc71c
kyunggwan <4562sky@naver.com>
2024-01-22 22:43:26 +0900

    chore: pc 코드 설치 및 패키지 정리

--- board-front/src/apis/response/auth/sign-up.response.dto.ts
+++ board-front/src/apis/response/auth/sign-up.response.dto.ts
@@ -1,5 +1,5 @@
 import ResponseDto from "../response.dto";
 
-export default interface SignUpResponseDto extends ResponseDto{
+export default interface SignUpResponseDto extends ResponseDto {
     
 }
