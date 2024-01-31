c6cfa6c49d4ef81858587050df0e2c6bf6ff6357
kyunggwan <4562sky@naver.com>
2024-02-01 01:47:08 +0900

    feat: 페이징 컴포넌트 작업 중

--- board-front/src/components/Pagination/index.tsx
+++ board-front/src/components/Pagination/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./style.css";
 
-//              component: 페이지네이션 컴포넌트                //
+//        component: 페이지네이션 컴포넌트        //
 export default function Pagination() {
-  //              render: 페이지네이션 컴포넌트 렌더링                //
-  return <div></div>;
+  //        render: 페이지네이션 컴포넌트 렌더링        //
+  return <div id='pagination-wrapper'>
+    <div className="pageination-change-link-box"></div>
+    <div className="pagination-divider">{'\|'}</div>
+    
+    <div className="pagination-divider">{'\|'}</div>
+    <div className="pagination-change-link-box"></div>
+  </div>;
 }
