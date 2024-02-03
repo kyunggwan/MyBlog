c9291793c17f35087a39ecad17940b7589551476
kyunggwan <4562sky@naver.com>
2024-02-03 23:58:00 +0900

    feat: 페이징 처리(Pagination) 컴포넌트 적용 중

--- board-front/src/apis/response/board/get-comment-list.response.dto.ts
+++ board-front/src/apis/response/board/get-comment-list.response.dto.ts
@@ -2,5 +2,5 @@ import { CommentListItem } from "types/interface";
 import ResponseDto from "../response.dto";
 
 export default interface GetCommentListResponseDto extends ResponseDto {
-    commentList: CommentListItem[];
+    commentListItem: CommentListItem[];
 }
