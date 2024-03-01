1d5353b5a1080aa551db5151defd8484adb40e1d
kyunggwan <4562sky@naver.com>
2024-03-02 00:41:44 +0900

    style: 검색어 페이지 스타일 작성

--- board-front/src/views/Search/index.tsx
+++ board-front/src/views/Search/index.tsx
@@ -12,20 +12,21 @@ export default function Search() {
   //        state: searchWord path Variable 상태        //
   const { searchWord } = useParams();
   //        state: 검색 게시물 갯수 상태        //
-  const [count, setCount] = useState<number>(2);
+  const [count, setCount] = useState<number>(0);
   //        state: 검색 게시물 리스트 상태(임시)        //
   const [searchBoardList, setSearchBoardList] = useState<BoardListItem[]>([]);
   //        state: 연관 검색어 리스트 상태        //
   const [relationList, setRelationList] = useState<string[]>([]);
   //        function: 네비게이트 함수        //
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   //        event handler: 연관 검색어 클릭 이벤트 처리        //
   const onRelationWordClickHandler = (word: string) => {
     navigate(SEARCH_PATH(word));
-  }
+  };
   //        effect: 첫 마운트 시 실행될 함수        //
   useEffect(() => {
     setSearchBoardList(latestBoardListMock);
+    setRelationList([]);
   }, [searchWord]);
   //        render: 게시물 검색 화면 컴포넌트 렌더링        //
   if (!searchWord) return <></>;
@@ -34,8 +35,8 @@ export default function Search() {
       <div className="search-container">
         <div className="search-title-box">
           <div className="search-title">
-            <span className="emphasis">{searchWord}</span> 에 대한 검색결과
-            입니다.
+            <span className="search-title-emphasis">{searchWord}</span> 에 대한
+            검색결과 입니다.
           </div>
           <div className="search-count">{count}</div>
         </div>
@@ -55,21 +56,35 @@ export default function Search() {
           <div className="search-relation-box">
             <div className="search-relation-card">
               <div className="search-relation-card-container">
-                <div className="search-relation-card-title"></div>
-                
-                {relationList.length === 0 ?
-                <div className="search-relation-card-contents-nothing"></div> :
-                <div className="search-relation-card-contents">
-                    {relationList.map(word => <div className="word-badge" onClick={() => onRelationWordClickHandler(word)}>{word}</div>)}
+                <div className="search-relation-card-title">
+                  {"관련 검색어"}
                 </div>
-                }
-                
+
+                {relationList.length === 0 ? (
+                  <div className="search-relation-card-contents-nothing">
+                    {"관련 검색어가 없습니다."}
+                  </div>
+                ) : (
+                  <div className="search-relation-card-contents">
+                    {relationList.map((word) => (
+                      <div
+                        className="word-badge"
+                        onClick={() => onRelationWordClickHandler(word)}
+                      >
+                        {word}
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
             </div>
           </div>
         </div>
         <div className="search-pagination-box">
-            {/* <Pagination /> */}
+          {count !== 0 &&
+            {
+              /* <Pagination />*/
+            }}
         </div>
       </div>
     </div>
